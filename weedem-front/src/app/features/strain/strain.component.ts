90268bc0bf73e650f66428375647e350adad7e18
Broitman <gabrielbroitmanpinheiro@gmail.com>
2019-02-07 01:42:36 -0200

    estruturando modulos

---
+++ weedem-front/src/app/features/strain/strain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-strain',
+  templateUrl: './strain.component.html',
+  styleUrls: ['./strain.component.css']
+})
+export class StrainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ weedem-front/src/app/features/strain/strain.routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { StrainComponent } from './strain.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [StrainComponent],
+  providers: [],
+})
+export class StrainModule { }
